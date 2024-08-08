5f234393fa9d44f85e876521a4699346a46fb5ea
ROHAN GROVER <rohangrover@ROHANs-MacBook-Air.local>
2024-08-08 16:07:23 -0400

    added gradio changes and simplified functions

--- agents.py
+++ agents.py
@@ -4,6 +4,9 @@ from langchain.agents import initialize_agent, load_tools
 from langchain_openai import OpenAI
 from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
 import logging
+import openai
+import gradio as gr
+import datetime
 
 # create the chatbot class
 class agents():
@@ -22,10 +25,14 @@ class agents():
         try:
             if self.open_ai_key_str==None:
                 return("please add you open_ai key to proceed")
-        
+
+
             self.llmObj = OpenAI(
-                 temperature=0.9
-                 )
+                temperature=0.9
+            )
+            print(self.llmObj)
+            print(self.llmObj.model_name)
+
             
             tools = load_tools(["dalle-image-generator"])
             self.dalle_agent = initialize_agent(tools, self.llmObj, agent="zero-shot-react-description", verbose=True)
@@ -52,9 +59,84 @@ class agents():
                      os.path.basename(__file__), sys.exc_info()[-1].tb_lineno, type(e).__name__, str(e)))
 
 
-
+def match_res_n_model(model,resolution,n):
+    if model == "dall-e-2":
+        n = gr.Slider(minimum=1, maximum=10, step=1, label="Images Requested", value=1, interactive=True)
+        return gr.Radio(choices=["256x256", "512x512", "1024x1024"], label="Image Resolution", value="256x256") , n
+    else:
+        n = gr.Number(value=1, maximum=1, minimum=1, label="Images Requested")
+        return gr.Radio(choices=[ "1024x1024"], label="Image Resolution", value="1024x1024"), n
+        
+        
+def generate_images(openai_api_key, output_file_path, prompt, n, resolution, model, style, quality):
+    openai.api_key = openai_api_key
+    response_text = "Submitting image creation request to OpenAI with the following parameters:\n"
+    response_text += f"Prompt: {prompt}\nNumber of images: {n}\nImage resolution: {resolution}\n\n"
+    
+    try:
+        response = openai.images.generate(
+            model=model,
+            quality=quality,
+            style=style,
+            prompt=prompt,
+            n=n,
+            size=resolution
+        )
+        
+        response_text += "Response:\n"
+        images_urls = []
+        
+        for idx in range(0, n):
+            url = response.data[idx].url
+            images_urls.append(url)
+            response_text += f"URL for Image #{idx}: {url}\n"
+        
+        # Optionally save the response to a file
+        if output_file_path != "":   
+            with open(output_file_path, "a") as f:
+                now = datetime.datetime.now()
+                date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+                f.write("-----------------------------\n")
+                f.write(date_time + "\n")
+                f.write(response_text)
+                for url in images_urls:
+                    f.write(url + "\n")
+        
+        return response_text, images_urls
+    except openai.OpenAIError as e:
+        return f"Error: {str(e)}", []
 
             
+def generate_image_v2(openai_api_key, output_file_path, prompt):
+    try:
+        os.environ["OPENAI_API_KEY"] = openai_api_key
+        llmObj = OpenAI(
+                temperature=0.9
+            )
+        tools = load_tools(["dalle-image-generator"])
+        dalle_agent = initialize_agent(tools, llmObj, agent="zero-shot-react-description", verbose=True)
+        logging.info("DALL-E agent initialized")
+
+        response_text = dalle_agent.run(prompt)
+
+        print(f"RESPONSE:\n{response_text}")
+
+        image_url = DallEAPIWrapper().run(response_text)
+
+        #  # Optionally save the response to a file
+        # if output_file_path != "":   
+        #     with open(output_file_path, "a") as f:
+        #         now = datetime.datetime.now()
+        #         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        #         f.write("-----------------------------\n")
+        #         f.write(date_time + "\n")
+        #         f.write(response_text)
+        #         f.write(image_url + "\n")
+        
+        return response_text, image_url
+    except Exception as e:
+            logging.error('file:{} line:{} type:{}, message:{}'.format(
+                     os.path.basename(__file__), sys.exc_info()[-1].tb_lineno, type(e).__name__, str(e)))
         
 
 
--- app.py
+++ app.py
@@ -5,40 +5,81 @@ from langchain.prompts import ChatPromptTemplate
 import gradio as gr
 from agents import agents
 import logging
-
+from agents import match_res_n_model, generate_images, generate_image_v2
      
 # # get the open_ai api key
 # _ = load_dotenv(find_dotenv()) # read local .env file
 # openai_api_key_str = os.environ['OPENAI_API_KEY']
 # print(openai_api_key_str)
 
-llm_model = "dall-e-2"
+llm_model = "dall-e-3"
 agentObj = agents(llm_model=llm_model)
 
 if __name__ == "__main__":
     try:
+
         # setup the gradio chatbot
         with gr.Blocks() as demo:
+            gr.Markdown("## DALL-E Image Creation")
             with gr.Row():
-                with gr.Column(scale=1, variant="compact"):
-                    gr.Markdown("Enter you open API key")
-                    inp = gr.Textbox(placeholder="open API Key")                
-                    btna = gr.Button("Enter")
-                btna.click(fn=agentObj.set_open_ai_key, inputs=inp)    
-
+                openai_api_key = gr.Textbox(label="OpenAI Key", type="password", value="", placeholder="Enter your OpenAI API key...")
+                output_file_path = gr.Textbox(label="Save Log File Path (optional)", placeholder="Enter the file path to save the log file...", type="text")
 
             with gr.Row():
-                out_x=gr.Textbox(label="status", scale=1),
-            btn1 = gr.Button("Initialize DALL-E Agent", scale=1)    
-            btn1.click(fn=agentObj.initialize_dalle_agent, outputs=out_x[0])    
+                prompt = gr.Textbox(label="Prompt", placeholder="Enter your prompt here...")
 
+            generate_button = gr.Button("Generate")
+            
             with gr.Row():
-                inputs_x=gr.Textbox(label="image description"),
-                outputs_x=gr.Image(label="DALL-E Image"),
-            btn2 = gr.Button("Run")
-            btn2.click(fn=agentObj.generate_image, inputs=inputs_x[0], outputs=outputs_x[0])
+                result_text = gr.Textbox(label="Response output:", lines=10, interactive=False)
+                images_output = gr.Image(label="Generated Image")
 
-        demo.launch()
+            # if the model changes to "dall-e-3", we need to change the resolution and n
+            generate_button.click(
+                generate_image_v2,
+                inputs=[openai_api_key, output_file_path, prompt],
+                outputs=[result_text, images_output]
+            )
+        
+            # with gr.Row():
+            #     inputs_x=gr.Textbox(label="image description"),
+            #     outputs_x=gr.Image(label="DALL-E Image"),
+            # btn2 = gr.Button("Run")
+            
+            # btn2.click(fn=agentObj.generate_image, inputs=inputs_x[0], outputs=outputs_x[0])
+
+        demo.launch(share=True)
+        
+
+        # # Gradio interface components
+        # with gr.Blocks() as demo:
+        #     gr.Markdown("## DALL-E Image Creation")
+        #     with gr.Row():
+        #         openai_api_key = gr.Textbox(label="OpenAI Key", type="password", value=key, placeholder="Enter your OpenAI API key...")
+        #         output_file_path = gr.Textbox(label="Save Log File Path (optional)", placeholder="Enter the file path to save the log file...", type="text")
+        #     with gr.Row():
+        #         prompt = gr.Textbox(label="Prompt", placeholder="Enter your prompt here...")
+        #         n = gr.Slider(minimum=1, maximum=10, step=1, label="Images Requested", value=1)
+        #         model = gr.Radio(choices=["dall-e-2", "dall-e-3"], label="Model", value="dall-e-2")
+        #         resolution = gr.Radio(choices=["256x256", "512x512", "1024x1024"], label="Image Resolution", value="256x256")
+        #     with gr.Row():
+        #         quality = gr.Radio(choices=["standard", "hd"], label="Quality", value="standard")
+        #         style = gr.Radio(choices=["natural", "vivid"], label="Style", value="natural")
+                
+        #     generate_button = gr.Button("Generate")
+            
+        #     with gr.Row():
+        #         result_text = gr.Textbox(label="Response output:", lines=10, interactive=False)
+        #         images_output = gr.Gallery(label="Generated Images")
+
+        #     # if the model changes to "dall-e-3", we need to change the resolution and n
+        #     model.change(match_res_n_model, [model,resolution,n],[resolution,n])
+        #     generate_button.click(
+        #         generate_images,
+        #         inputs=[openai_api_key, output_file_path, prompt, n, resolution, model, style, quality],
+        #         outputs=[result_text, images_output]
+        #     )
+        # demo.launch(share=True)
 
     except Exception as e:
             logging.error('file:{} line:{} type:{}, message:{}'.format(
